e3a96633f955c9e7c550528fd186f3a2d8a73ce5
SaranaMaxim <maksimsarana8@gmail.com>
2023-12-12 09:47:40 +0300

    correct

--- FPBot2/main.py
+++ FPBot2/main.py
@@ -1,6 +1,8 @@
-import telebot
+import telebot 
 from telebot import types
 bot = telebot.TeleBot('6605974541:AAHK8C1ZwDC4JpTNqfC-Fvxhm4ucCqJnbLo')
+
+gamelist=['ARK: Survival Evolved', 'Black Russia', 'Brawl Stars', 'CarX Drift Racing', 'Counter-Strike 2', 'Crossout', 'Cyberpunk 2077', 'Dead by Daylight', 'Dota 2', 'Dying Light 2', 'Hearts of Iron IV', 'Minecraft', 'Mob', 'PUBG', 'PUBG Mobile', 'Radmir', 'Rust', 'War Thunder', 'Warface', 'Woorld of Tanks', 'World of Tanks Blitz','YouTube', 'Telegram Premium', 'ВК', 'Netflix']
 
 @bot.message_handler(commands=['start'])
 def cmd_start(message):
@@ -12,7 +14,7 @@ def cmd_start(message):
     bot.register_next_step_handler(message, on_click)
 def on_click(message):
     if message.text == 'Выбрать игру':
-        bot.send_message(message.chat.id, 'Напишите название в чат одним словом. Пример: dota2')
+        bot.send_message(message.chat.id, 'Напишите название в чат так, как она указана в списке игр, чтобы посмотреть список игр /help')
 
 @bot.message_handler(commands=['help'])
 def cmd_start(message):
@@ -25,6 +27,7 @@ def callback_message(callback):
     if callback.data == 'help':
         bot.send_message(callback.message.chat.id, 'В перечень входят такие игры как: ARK: Survival Evolved, Black Russia, Brawl Stars, CarX Drift Racing, Counter-Strike 2, Crossout, Cyberpunk 2077, Dead by Daylight, Dota 2, Dying Light 2, Hearts of Iron IV, Minecraft, Mobile Legends, PUBG, PUBG Mobile, Radmir, Rust, War Thunder, Warface, World of Tanks, World of Tanks Blitz. Так-же доступен парсинг каналов YouTube, Telegram Premium, Подписок на ВК и Кинотеатры')
 
+ 
 
 
 
